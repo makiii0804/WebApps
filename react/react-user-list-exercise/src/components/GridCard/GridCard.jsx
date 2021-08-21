a3b43b5cea95f110009b65f5c8d9a11d4864e904
marijakov0804 <marija.kovacevic.0804@gmail.com>
2021-08-21 11:28:38 +0200

    user list exercise added grid card component

--- react/react-user-list-exercise/src/components/GridCard/GridCard.jsx
+++ react/react-user-list-exercise/src/components/GridCard/GridCard.jsx
@@ -1,3 +1,24 @@
-import React from 'react';
+import React from "react";
+import "../User/User"
+import "../../data/random-user"
 
-export const GridCard = () => <h1>THIS IS CARD</h1>;
+import "./GridCard.css"
+
+const GridCard = (props) => {
+
+    return (
+        <React.Fragment>
+            <div className="img-container">
+                <img src={props.userData.picture.medium} alt="" />
+                <h4>{props.userData.name.first}</h4>
+            </div>
+            <div className="data-container">
+                <p>Name: {props.userData.email}</p>
+                <p>Date of birth: {props.userData.dob.date}</p>
+            </div>
+        </React.Fragment>
+
+    )
+}
+
+export { GridCard };
