e0bb2960e46e1c1e943948cacafee3a1cf54213c
marijakov0804 <marija.kovacevic.0804@gmail.com>
2021-08-21 15:59:47 +0200

    blog list exercise(modified form/input/search) finished

--- react/react-blog-list/src/components/BlogList/BlogList.jsx
+++ react/react-blog-list/src/components/BlogList/BlogList.jsx
@@ -1,50 +1,75 @@
-import React, { Component } from "react";
-// import { BlogPosts } from "../../data/blog-posts";
+import React, { useState, useEffect } from "react";
 import { getBlogs } from "../../services/blogService";
 
 import './BlogList.css'
 
-export class BlogList extends Component {
-    constructor (props) {
-        super(props)
-        this.state = {
-            blogs: []
-        }
+export const BlogList = () => {
+    const [blogs, setBlogs] = useState([])
+    const [filteredBlogs, setFilteredBlogs] = useState ([])
+    const [inputValue, setInputValue] = useState ("")
+
+
+    useEffect (() => {
+        getBlogs ().then((blogs) => {
+            setBlogs(blogs);
+            setFilteredBlogs(blogs)
+        });
+    },[])
+
+    const onTyping = (e) => {
+        setInputValue (e.target.value)
+        const fBlogs = blogs.filter(blog => blog.title.includes(inputValue))
+        setFilteredBlogs(fBlogs)
     }
-    
-    componentDidMount () {
-        getBlogs()
-        .then(blogs => {
-            this.setState({blogs})
-        })
-    }
-//    blogs = fetchBlogs()
-    render () {
-        if(this.state.blogs.length === 0) return <div>  Loading....</div>
-        return (
-            <div className='blogList'>
-                {this.state.blogs.map((post, index) => (
-                   <div className='blogItem' key={index}>
-                       <h2>{post.title}</h2>
-                       <p>{post.body}</p>
-    
-                   </div>
-                ))}
-            </div>
-        ); 
-    }
+
+    if (blogs.length === 0) return <div>  Loading....</div>
+    return (
+        <div className='blogList'>
+            <input value = {inputValue} type="text" onChange = {onTyping}/>
+            {filteredBlogs.map((post, index) => (
+               <div className='blogItem' key={index}>
+                   <h2>{post.title}</h2>
+                   <p>{post.body}</p>
+
+               </div>
+            ))}
+        </div>
+    ); 
 }
 
-// export const BlogList = () => {
-//     return (
-//         <div className='blogList'>
-//             {BlogPosts.map((post, index) => (
-//                <div className='blogItem' key={index}>
-//                    <h2>{post.title}</h2>
-//                    <p>{post.body}</p>
 
-//                </div>
-//             ))}
-//         </div>
-//     );
-// };
+
+
+
+
+
+
+// export class BlogList extends Component {
+//     constructor (props) {
+//         super(props)
+//         this.state = {
+//             blogs: []
+//         }
+//     }
+    
+//     componentDidMount () {
+//         getBlogs()
+//         .then(blogs => {
+//             this.setState({blogs})
+//         })
+//     }
+//     render () {
+        // if(this.state.blogs.length === 0) return <div>  Loading....</div>
+        // return (
+        //     <div className='blogList'>
+        //         {this.state.blogs.map((post, index) => (
+        //            <div className='blogItem' key={index}>
+        //                <h2>{post.title}</h2>
+        //                <p>{post.body}</p>
+    
+        //            </div>
+        //         ))}
+        //     </div>
+        // ); 
+//     }
+// }
